075ff1b684cc43b3260f79adf04024bd5a634383
Finity <bgruda223@gmail.com>
2023-05-08 15:35:06 +0200

    Added optimised memory LCS_Length calculation

--- LCS.h
+++ LCS.h
@@ -6,6 +6,58 @@
 
 using namespace std;
 
+int Minimal_memory_LCS_length(const string& A, const string& B) {
+    size_t m = A.size();
+    size_t n = B.size();
+
+    if (n > m) {
+        return Minimal_memory_LCS_length(B, A); // Swap A and B to ensure that A is the smaller string
+    }
+
+    vector<int> dp(n + 1, 0);
+    int prev_diag = 0;
+
+    for (size_t i = 1; i <= m; i++) {
+        for (size_t j = 1; j <= n; j++) {
+            int temp = dp[j]; // Store the current value of dp[j] before updating it
+            if (A[i - 1] == B[j - 1]) {
+                dp[j] = prev_diag + 1;
+            }
+            else {
+                dp[j] = max(dp[j], dp[j - 1]);
+            }
+            prev_diag = temp; // Update prev_diag for the next iteration
+        }
+        prev_diag = 0; // Reset prev_diag for the next row
+    }
+
+    return dp[n];
+}
+
+int Reduced_memory_LCS_length(const string& A, const string& B) {
+    size_t m = A.size();
+    size_t n = B.size();
+
+    if (n > m) {
+        return Reduced_memory_LCS_length(B, A); // Swap A and B to ensure that A is the smaller string
+    }
+
+    vector<vector<int>> dp(2, vector<int>(n + 1, 0));
+
+    for (size_t i = 1; i <= m; i++) {
+        for (size_t j = 1; j <= n; j++) {
+            if (A[i - 1] == B[j - 1]) {
+                dp[i % 2][j] = dp[(i - 1) % 2][j - 1] + 1;
+            }
+            else {
+                dp[i % 2][j] = max(dp[(i - 1) % 2][j], dp[i % 2][j - 1]);
+            }
+        }
+    }
+
+    return dp[m % 2][n];
+}
+
 // Function to find the longest common subsequence of two strings
 string LCS(const string& A, const string& B) {
     size_t m = A.size();
--- main.cpp
+++ main.cpp
@@ -4,10 +4,18 @@ int main() {
     string A = "This is some text";
     string B = "Here is text too";
     string result = LCS(A, B);
-    //std::string result = LCS_Diffing(A, B);
+    int reduced_result = Reduced_memory_LCS_length(A, B);
+    int minimal_result = Minimal_memory_LCS_length(A, B);
+    //string result = LCS_Diffing(A, B);
     cout << "Testing the LCS: \n\n";
     cout << "Result: \n" << "|" << result << "|" << '\n';
-    cout << "\nthe \"|\" indicators mark the beginning and the end of the LCS (make spaces visible)\n";
+    cout << "\nthe \"|\" indicators mark the beginning and the end of the LCS (makes spaces visible)\n";
+
+    cout << "\nTesting the Reduced Memory LCS to calculate lenght O( 2*min(m,n) )\n";
+    cout << reduced_result << '\n';
+
+    cout << "\nTesting the Minimal Memory LCS to calculate lenght O( min(m,n ) )\n";
+    cout << minimal_result << '\n';
 
     cout << "\nTesting the LCS_Diffing\n\n";
     LCS_Diffing("TestFile1.txt", "TestFile2.txt");
